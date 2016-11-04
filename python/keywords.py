6518a8489ca538ab882b4435569019b6a6e148e8
Mercerenies <sdmayolo42@students.tntech.edu>
2016-11-04 14:59:12 -0500

    Made the static class into a singleton class

--- python/keywords.py
+++ python/keywords.py
@@ -1,9 +1,14 @@
 
+import sys
+
 class Keywords:
-    _key = None
-    @staticmethod
-    def _load_file():
-        Keywords._key = dict()
+    _inst = None
+
+    def __init__(self):
+        self.key = None
+
+    def _load_file(self):
+        self.key = dict()
         state = ""
         with open("./data/keywords.txt") as f:
             data = f.read()
@@ -13,16 +18,27 @@ class Keywords:
             if curr[:3] == ":: ":
                 state = curr[3:]
             else:
-                Keywords._key[state] = Keywords._key.get(state, []) + [curr]
-    @staticmethod
-    def check_match(key, c):
+                self.key[state] = self.key.get(state, []) + [curr]
+
+    def match(self, key, c):
         """
         Attempt to match the category c against the match words associated with the keyword key
-        provided. Returns whether any matches were found
+        provided. Returns whether any matches were found.
         """
-        if not Keywords._key:
-            Keywords._load_file()
-        for x in Keywords._key[key]:
+        if not self.key:
+            self._load_file()
+        for x in self.key[key]:
             if x in c.lower(): # TODO Whole words only? Consider the consequences if we switched to that
                 return True
         return False
+
+    @staticmethod
+    def instance():
+        return Keywords._inst
+
+    @staticmethod
+    def check_match(key, c):
+        """A convenience method which calls match on the default Keywords instance."""
+        return Keywords.instance().match(key, c)
+
+Keywords._inst = Keywords()

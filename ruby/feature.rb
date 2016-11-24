9af61c8e79d31ef4299463df7f7aa10bdcc7e817
Mercerenies <sdmayolo42@students.tntech.edu>
2016-11-24 16:42:52 -0600

    Some much needed documentation on the structure nodes

--- ruby/feature.rb
+++ ruby/feature.rb
@@ -1,30 +1,42 @@
 
+# A collection of locations that form a cohesive structure when taken together. Note that features are intended
+# to be finalized structures, not modifiable data, which is why there are relatively few editing methods
+# provided in this interface. Features should be built up using #StructureBuilder or similar interfaces
+# and then compiled.
 class Feature
 
+  # Constructs an empty feature, consisting of no nodes.
   def initialize
     @nodes = []
     @exits = []
   end
 
-  def load(node)
+  # Given a data instance from a page, loads the structure from the information on the page.
+  def load(data)
   end
 
+  # Adds a collection of locations to the world feature.
   def push(*xs)
     xs.each { |x| @nodes.push x }
   end
 
+  # Pushes a location onto the exit list of the feature. The exit nodes should always form a subset of the set
+  # of nodes. If #push_exit is passed a node which is not in the node set, it is a no-op.
   def push_exit(x)
-    @exits.push x
+    @exits.push x if @nodes.find? x
   end
 
+  # Accesses the nth node in the node list, returning +nil+ if out of bounds.
   def [](n)
     @nodes[n]
   end
 
+  # Iterates over the node list.
   def each_node(&block)
     @nodes.each &block
   end
 
+  # Iterates over the exit list.
   def each_exit(&block)
     @exits.each &block
   end
--- ruby/structure.rb
+++ ruby/structure.rb
@@ -1,6 +1,10 @@
 
 require 'forwardable'
 
+# A node in a structure that is being built. When the structure is ready, #to_loc will return a location
+# instance that can be placed on the map. The StructureNode class is intended to be subclassed and the
+# #expand method overriden to define custom behavior for various structures. For this reason, several
+# of the #StructureBuilder methods are redefined in this class to delegate to the builder object.
 class StructureNode
   extend Forwardable
 
@@ -9,6 +13,7 @@ class StructureNode
   def_delegators :@builder, :make_node, :connect, :add_names, :get_a_name, :sample_node, :sample_nodes,
                             :core_name=, :core_name, :each_node, :select_nodes, :each_exit
 
+  # Initializes a node with a reference back to its builder instance.
   def initialize(builder, name)
     @builder = builder
     @id = Node.get_id
@@ -18,26 +23,33 @@ class StructureNode
     @plants = nil
   end
 
+  # Called during recursive expansion of the structure node to define the expansion for the individual node.
   def expand
   end
 
+  # Constructs a #Location instance from the structure node.
   def to_loc
     Location.new @id, @name, nil, generic_name: core_name, valid_creatures: @creatures, valid_plants: @plants
   end
 
+  # Returns whether the node is intended as an exit.
   def exit?
     @exit
   end
 
+  # Marks the structure node as an exit.
   def mark_as_exit
     @exit = true
   end
 
 end
 
+# A builder which consists of #StructureNode instances. Like #StructureNode, #StructureBuilder is intended
+# to be subclassed with #construct overriden to determine how to start the building process.
 class StructureBuilder
   attr_accessor :core_name
 
+  # Initializes an empty structure.
   def initialize
     @nodes = []
     @connections = []
@@ -45,23 +57,34 @@ class StructureBuilder
     @core_name = nil
   end
 
+  # Adds nodes to the structure.
   def add(*nodes)
     @nodes.push(*nodes)
   end
 
+  # Once a builder is allocated and initialized, #construct is intended to be called. The #construct method
+  # should call the other builder methods to initialize the construction and make the first few #StructureNode
+  # instances, which will then be automatically expanded by the helper methods here.
   def construct
   end
 
+  # Transforms the structure builder's internal state into a #StructureResult object, consisting of finalized
+  # locations and marked exit positions.
   def to_loc
     nodes = @nodes.collect(&:to_loc)
     @connections.each do |n0, n1|
       nodes.find { |n| n.id == n0.id }.add_link n1.id
     end
     exits = @nodes.select(&:exit?)
-    exits = exits.map { |e| nodes.find { |n| n.id == e.id } }
+    exits = exits.map { |e| nodes.find { |n| n.id == e.id } } # Verifies that @exits is a subset of @nodes
     StructureResult.new nodes, exits
   end
 
+  # Constructs a node of the given type, passing the arguments to the initializer. When using the
+  # +StructureBuilder+ interface, calling through to #make_node is preferable to directly calling
+  # the constructors. The #make_node method constructs the +StructureNode+ derivative, adds it to
+  # the structure builder's state, and then expands the node. The consequences are undefined if
+  # this method is called with a type which is not a +StructureNode+ instance or child.
   def make_node(type, *args)
     elem = type.new self, *args
     @nodes << elem
@@ -69,23 +92,32 @@ class StructureBuilder
     elem
   end
 
+  # Connects the two nodes in the structure. Using this method, the exits to the node are added
+  # symmetrically, creating a two-way link.
   def connect(node0, node1)
     @connections << [node0, node1] unless @connections.include? [node0, node1]
     @connections << [node1, node0] unless @connections.include? [node1, node0]
   end
 
+  # Adds names to the builder's internal name data. These names are not automatically used; they
+  # are retrieved only when #get_a_name is called.
   def add_names(*names)
     @names.push(*names)
   end
 
+  # Retrieves and removes a name from the builder's name data, or +nil+ if there are no names left.
   def get_a_name
     @names.sample.tap { |n| @names.delete n }
   end
 
+  # Returns a random node from the builder's state. This method does not remove the node.
   def sample_node
     @nodes.sample
   end
 
+  # Returns a collection of random nodes of the given size from the builder's state. Nodes will not be
+  # repeated in the returned list. If the number requested exceeds the number of available nodes,
+  # a list containing all of the nodes is returned.
   def sample_nodes(n)
     if n >= @nodes.length
       @nodes.dup
@@ -99,20 +131,24 @@ class StructureBuilder
     end
   end
 
+  # Iterates over the nodes in the builder.
   def each_node(&block)
     @nodes.each &block
   end
 
+  # Filters the nodes in the builder, returning an enumerable.
   def select_nodes(&block)
     each_node.select &block
   end
 
+  # Iterates over the nodes in the builder which have been marked as exits.
   def each_exit(&block)
     @nodes.select(&:exit?).each &block
   end
 
 end
 
+# A simple structure which is returned when a #StructureBuilder finishes constructing its data.
 class StructureResult
   attr_reader :nodes, :exits
 

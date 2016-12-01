274ef8c411845483bda8489f713c0f84d43a2139
Mercerenies <sdmayolo42@students.tntech.edu>
2016-12-01 14:17:16 -0600

    Refactored the Page class hierarchy

--- ruby/loader.rb
+++ ruby/loader.rb
@@ -1,6 +1,55 @@
 #!/usr/bin/ruby
 
+module TwoColumnData
+
+  def load_two_column(data)
+    data.map { |xx| [xx[1], xx[0].intern] }.to_h
+  end
+
+  def save_two_column(data)
+    data.map { |k, v| [v, k] }
+  end
+
+  private :load_two_column, :save_two_column
+
+end
+
+module HasInfoField
+  include TwoColumnData
+
+  attr_reader :info
+
+  def initialize_info(data)
+    @info = load_two_column data
+  end
+
+  def save_info_field
+    save_two_column @info
+  end
+
+  def type
+    key = keyword
+    key and @info[key]
+  end
+
+  def keyword
+    freq = @info.each_with_object(Hash.new(0)) do |(key, val), hash|
+      hash[val] += 1
+    end
+    mode = freq.max_by { |k, v| v }
+    mode and @info.detect { |k, v| v == mode[0] }[0]
+  end
+
+  private :initialize_info, :save_info_field
+
+end
+
 class Page
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
 
   def to_json(options = {})
     "{}"
@@ -9,116 +58,80 @@ class Page
 end
 
 class PersonPage < Page
-  attr_reader :name, :gender, :occupations
+  include TwoColumnData
+  attr_reader :gender, :occupations
 
   def initialize(json)
-    @name = json['name']
+    super json['name']
     @gender = json['gender']
     @gender = @gender.intern if @gender
-    @occupations = json['occupations'].map { |xx| [xx[1], xx[0].intern] }.to_h
+    @occupations = load_two_column json['occupations']
   end
 
   def to_json(options = {})
     {
       'nature' => 'Person',
-      'name' => @name,
+      'name' => name,
       'gender' => @gender,
-      'occupations' => @occupations.map { |k, v| [v, k] }
+      'occupations' => save_two_column(@occupations)
     }.to_json options
   end
 
 end
 
 class PlacePage < Page
-  attr_reader :name
+  include HasInfoField
 
   def initialize(json)
-    @name = json['name']
-    @info = json['info'].map { |xx| [xx[1], xx[0].intern] }.to_h
-  end
-
-  def type
-    key = keyword
-    key and @info[key]
-  end
-
-  def keyword
-    freq = @info.each_with_object(Hash.new(0)) do |(key, val), hash|
-      hash[val] += 1
-    end
-    mode = freq.max_by { |k, v| v }
-    mode and @info.detect { |k, v| v == mode[0] }[0]
+    super json['name']
+    initialize_info json['info']
   end
 
   def to_json(options = {})
     {
       'nature' => 'Place',
-      'name' => @name,
-      'info' => @info.map { |k, v| [v, k] }
+      'name' => name,
+      'info' => save_info_field
     }.to_json options
   end
 
 end
 
 class WeaponPage < Page
-  attr_reader :name
+  include HasInfoField
 
   def initialize(json)
-    @name = json['name']
-    @info = json['info'].map { |xx| [xx[1], xx[0].intern] }.to_h
-  end
-
-  def type
-    key = keyword
-    key and @info[key]
-  end
-
-  def keyword
-    freq = @info.each_with_object(Hash.new(0)) do |(key, val), hash|
-      hash[val] += 1
-    end
-    mode = freq.max_by { |k, v| v }
-    mode and @info.detect { |k, v| v == mode[0] }[0]
+    super json['name']
+    initialize_info json['info']
   end
 
   def to_json(options = {})
     {
       'nature' => 'Weapon',
-      'name' => @name,
-      'info' => @info.map { |k, v| [v, k] }
+      'name' => name,
+      'info' => save_info_field
     }.to_json options
   end
 
 end
 
 class MonsterPage < Page
-  attr_reader :name, :chaos, :affinity
+  include HasInfoField
+
+  attr_reader :chaos, :affinity
 
   def initialize(json)
-    @name = json['name']
-    @info = json['type'].map { |xx| [xx[1], xx[0].intern] }.to_h
+    super json['name']
+    initialize_info json['type']
     @chaos = json['chaos'].intern
     @affinity = json['affinity'].intern
-  end
-
-  def type
-    key = keyword
-    key and @info[key]
-  end
-
-  def keyword
-    freq = @info.each_with_object(Hash.new(0)) do |(key, val), hash|
-      hash[val] += 1
-    end
-    mode = freq.max_by { |k, v| v }
-    mode and @info.detect { |k, v| v == mode[0] }[0]
   end
 
   def to_json(options = {})
     {
       'nature' => 'Monster',
-      'name' => @name,
-      'info' => @info.map { |k, v| [v, k] },
+      'name' => name,
+      'type' => save_info_field,
       'chaos' => @chaos,
       'affinity' => @affinity
     }.to_json options
@@ -127,10 +140,10 @@ class MonsterPage < Page
 end
 
 class AnimalPage < Page
-  attr_reader :name, :pack, :speed, :sea, :air, :threat, :size
+  attr_reader :pack, :speed, :sea, :air, :threat, :size
 
   def initialize(json)
-    @name = json['name']
+    super json['name']
     @pack = json['pack']
     @speed = json['speed']
     @sea = json['sea']
@@ -143,7 +156,7 @@ class AnimalPage < Page
   def to_json(options = {})
     {
       'nature' => 'Animal',
-      'name' => @name,
+      'name' => name,
       'pack' => @pack,
       'speed' => @speed,
       'sea' => @sea,
@@ -157,10 +170,10 @@ class AnimalPage < Page
 end
 
 class FoodPage < Page
-  attr_reader :name, :full_name, :plant, :nutrition, :poison
+  attr_reader :full_name, :plant, :nutrition, :poison
 
   def initialize(json)
-    @name = json['nickname']
+    super json['nickname']
     @full_name = json['name']
     @plant = json['plant']
     @plant = @plant.intern if @plant
@@ -171,7 +184,7 @@ class FoodPage < Page
   def to_json(options = {})
     {
       'nature' => 'Food',
-      'nickname' => @name,
+      'nickname' => name,
       'name' => @full_name,
       'plant' => @plant,
       'nutrition' => @nutrition,

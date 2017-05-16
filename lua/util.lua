bb6e06a78017a8512e4c483b48a7995106f99263
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2017-05-16 18:16:58 -0500

    PGID identification function and centralized caching for /proc/self/stat

--- lua/util.lua
+++ lua/util.lua
@@ -4,6 +4,7 @@ util = P
 setmetatable(P, {__index = _G})
 
 local pid_cache = nil
+local pgid_cache = nil
 
 function P.exists(fname)
    local f = io.open(fname)
@@ -15,13 +16,28 @@ function P.exists(fname)
    return exists
 end
 
+local function load_caches()
+   local f = assert(io.open('/proc/self/stat', 'r'))
+   pid_cache = f:read("*number") -- Read the PID
+   while f:read(1) ~= ')' do end -- Ignore the name
+   f:read(3) -- Ignore the state
+   f:read("*number") -- Ignore the PPID
+   pgid_cache = f:read("*number") -- Read the PGID
+   io.close(f)
+end
+
 function P.pid()
    if pid_cache == nil then
-      local f = assert(io.open('/proc/self/stat', 'r'))
-      pid_cache = f:read("*number")
-      io.close(f)
+      load_caches()
    end
    return pid_cache
+end
+
+function P.pgid()
+   if pgid_cache == nil then
+      load_caches()
+   end
+   return pgid_cache
 end
 
 function P.execute(cmd)

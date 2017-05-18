6428710d3d477dbef9a43606dfea551459cb7181
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2017-05-17 23:10:07 -0500

    Documentation for the navigator

--- ruby/navigation.rb
+++ ruby/navigation.rb
@@ -25,6 +25,9 @@ class Navigator
 
 end
 
+# A NavLocationLens is a navigation lens examining zero or more locations. From this lens,
+# methods are provided to move to nearby locations and to seek out the contents of the
+# lens.
 class NavLocationLens
 
   def initialize(nav, arr)
@@ -32,16 +35,27 @@ class NavLocationLens
     @locs = arr
   end
 
+  # Returns a new NavLocationLens examining all nodes which are adjacent to at least one of
+  # the nodes in the current lens. Note that #surrounding will return all adjacent nodes in
+  # addition to the currently selected ones, while #adjacent only returns adjacent nodes.
   def adjacent
     arr = @locs.flat_map { |x| x.each_link.to_a }.flat_map { |x| @nav.map[x] }
     NavLocationLens.new @nav, arr.to_a
   end
 
+  # Returns a new NavLocationLens examining all nodes which are equal or adjacent to at
+  # least one of the nodes in the current lens. Note that #surrounding will return all
+  # adjacent nodes in addition to the currently selected ones, while #adjacent only
+  # returns adjacent nodes.
   def surrounding
     arr = @locs.flat_map { |x| x.each_link.to_a }.flat_map { |x| @nav.map[x] }
     NavLocationLens.new @nav, (self.to_a + arr.to_a)
   end
 
+  # Returns a new NavLocationLens examining all nodes which are at most +n+ links away from
+  # the current node. Note that calling #within with an argument of +0+ will return a duplicate
+  # of the current lens, and calling #within with an argument of +1+ is equivalent to calling
+  # #surrounding.
   def within(n)
     if n < 0
       NavLocationLens.new @nav, []
@@ -52,21 +66,28 @@ class NavLocationLens
     end
   end
 
+  # Returns a pruned version of the lens. It is not normally to do this explicitly unless
+  # the lens constructor is being explicitly called. All of the built-in lens operations
+  # automatically prune where necessary.
   def prune
     NavLocationLens.new @nav, @locs.uniq
   end
 
+  # Returns a NavContentsLens containing all of the objects in the selected locations.
   def contents
     objs = @locs.flat_map { |x| x.each.to_a }
     NavContentsLens.new @nav, objs
   end
 
+  # Returns the array of nodes that are being examined with this lens.
   def to_a
     @locs.dup
   end
 
 end
 
+# A NavContentsLens, usually constructed by NavLocationLens#contents, examines a collection
+# of objects at zero or more locations.
 class NavContentsLens
 
   def initialize(nav, arr)
@@ -74,27 +95,37 @@ class NavContentsLens
     @objs = arr
   end
 
+  # Returns a pruned version of the lens. It is not normally necessary to call this method
+  # explicitly, as the built-in lens operations prune where necessary by default.
   def prune
     NavContentsLens.new @nav, @objs.uniq
   end
 
+  # Detects the first of the selected objects for which the block returns truthy.
+  # If such an object exists, it is returned. Otherwise, +nil+ is returned by default.
   def detect(&block)
     @objs.detect &block
   end
 
+  # Detects the first of the selected objects which matches (according to #===) the
+  # argument object. If such an object exists, it is returned. Otherwise, +nil+ is
+  # returned.
   def has?(obj)
     detect { |x| case x when obj then x end }
   end
 
+  # Returns an array of the objects being examined with this lens.
   def to_a
     @objs.dup
   end
 
 end
 
-# An exception for errors in the integrity of the navigator
+# An exception for errors in the integrity of the navigator.
 class NavigatorError < StandardError
+
   def initialize(msg = "Error navigating the map")
     super
   end
+
 end

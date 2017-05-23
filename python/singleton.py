ea420bb273354b85d462a48379111d02d9c8d395
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2017-05-22 22:14:55 -0500

    Singleton metaclass

---
+++ python/singleton.py
@@ -0,0 +1,8 @@
+
+# Based on https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).call(*args, **kwargs)
+        return cls._instances[cls]

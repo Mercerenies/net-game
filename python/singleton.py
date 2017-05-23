ac596de1ca56bc58fd1878e9b749499249ab6e3c
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2017-05-22 22:15:47 -0500

    Docs for singleton

--- python/singleton.py
+++ python/singleton.py
@@ -1,6 +1,10 @@
 
 # Based on https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
 class Singleton(type):
+    """
+    This metaclass is used to create singleton classes. That is, a class whose metaclass is
+    Singleton will only ever have at most one instance of its type in existence at any time.
+    """
     _instances = {}
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:

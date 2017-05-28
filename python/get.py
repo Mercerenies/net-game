2c5bd06feb638ed916eb482cc7684c81399e0613
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2017-05-27 19:09:02 -0500

    Removed old legacy code

--- python/get.py
+++ python/get.py
@@ -11,50 +11,6 @@ import search
 import command
 from tokenizer import TokenizeError
 
-def make_sel(keyword, rein):
-    """Constructs a simple selector or reinforcement learning selector, depending on arguments."""
-    if rein:
-        return reinforcement.ReinLinkSelector(keyword)
-    else:
-        return links.NoDupLinkSelector()
-
-def produce_result(args, mod = (lambda x: x), selector = None, **key):
-    """
-    Runs all of the algorithms given in the Arguments object, using the basis modifier to augment each
-    basis. The basis modifier should be a 1-argument callable which takes a basis and returns a new
-    basis, and it should be side-effect free.
-    """
-    rein = args.rein()
-
-    parts = {}
-    for arg in args.standard_sequence():
-        if arg.count <= 0:
-            continue
-        sel = selector or make_sel(arg.selector, rein and arg.rein)
-        searcher = search.BasicSearch(basis = mod(arg.basis), number = arg.count, selector = sel, keys = key)
-        parts[arg.key] = searcher.run()
-    return xmlify.xmlify(parts)
-
-
-def standard_run(args):
-    print(ET.tostring(produce_result(args)).decode())
-
-def unit_run(args):
-    # TODO This function is pretty much unused. I think we can safely remove it.
-    unit = args.unit().strip()
-
-    def basis_transformer(b):
-        return b.with_page(lambda: unit)
-
-    print(ET.tostring(
-        produce_result(
-            args,
-            mod = basis_transformer,
-            selector = links.AbortLinkSelector(),
-            max_tries = 1
-        )
-    ).decode())
-
 def expr_run(args):
     try:
         exprs = command.read(args.expr())
@@ -69,8 +25,4 @@ def expr_run(args):
 if __name__ == '__main__':
     args = TopLevelArguments(sys.argv[1:])
     logger.set_global_debug_level(args.debug())
-    # TODO We should report an error if -u and -e are both supplied, probably.
-    if args.expr():
-        expr_run(args)
-    else:
-        standard_run(args)
+    expr_run(args)

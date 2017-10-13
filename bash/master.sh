0b4f04f6f97088ef89d6efda816d672ef4f34f28
Mercerenies <sdmayolo42@students.tntech.edu>
2017-10-12 23:13:48 -0500

    Bugfix for argument passing

--- bash/master.sh
+++ bash/master.sh
@@ -82,7 +82,7 @@ if [ -n "$stage2" ]; then
     $stage2 <"./temp/${prefix}1.txt" >"./temp/${prefix}2.txt"
 fi
 if [ -n "$stage3" ]; then
-    $stage3 "$small" "$debug" <"./temp/${prefix}2.txt" >"./temp/system.txt"
+    $stage3 $small $debug <"./temp/${prefix}2.txt" >"./temp/system.txt"
 fi
 if [ -n "$stage4" ]; then
     $stage4
